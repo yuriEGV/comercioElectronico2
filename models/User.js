64ab1b492752a28868a70658ae6016cc8d7c901b
yuriEGV <yguajardov@gmail.com>
2025-12-05 12:06:27 -0300

    Actualización del proyecto

--- models/User.js
+++ models/User.js
@@ -1,27 +1,24 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
-name: {
-type: String,
-required: [true, 'Please provide name'],
-},
-email: {
-type: String,
-required: [true, 'Please provide email'],
-unique: true,
-},
-password: {
-type: String,
-required: [true, 'Please provide password'],
-},
-role: {
-type: String,
-enum: ['user', 'admin'],
-default: 'user',
-},
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-const User = mongoose.model('User', UserSchema);
+// Hashear la contraseña antes de guardar
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-// Export default para ESModules
-export default User;
+// Método para comparar contraseña
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
+export default mongoose.model('User', UserSchema);
